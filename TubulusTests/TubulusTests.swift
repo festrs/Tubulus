5ab3515ec6c3ca3715f860e32559313da2c6172e
Felipe Dias Pereira <felipediaspereira@Felipes-MacBook-Pro.local>
2016-04-14 14:25:08 -0300

    pod libraries removed

--- TubulusTests/TubulusTests.swift
+++ TubulusTests/TubulusTests.swift
@@ -33,11 +33,11 @@ class TubulusTests: XCTestCase {
         }
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
 }
