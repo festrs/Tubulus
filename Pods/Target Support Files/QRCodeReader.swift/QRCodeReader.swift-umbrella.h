d5e378badb3dbab1c3f070ea918d35c6c2268f4e
festrs <felipe.dipereira@gmail.com>
2017-03-10 16:54:45 -0300

    renew

--- Pods/Target Support Files/QRCodeReader.swift/QRCodeReader.swift-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double QRCodeReaderVersionNumber;
-FOUNDATION_EXPORT const unsigned char QRCodeReaderVersionString[];
-
--- Pods/Target Support Files/RSBarcodes_Swift/RSBarcodes_Swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RSBarcodes_Swift : NSObject
-@end
-@implementation PodsDummy_RSBarcodes_Swift
-@end
